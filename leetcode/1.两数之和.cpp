09c4967ad1c8f66952c788116828e6f31426903d
unknown <1245215743@qq.com>
2021-05-31 23:34:54 +0800

    leetcode

--- leetcode/1.两数之和.cpp
+++ leetcode/1.两数之和.cpp
@@ -3,26 +3,48 @@
  *
  * [1] 两数之和
  */
-// #include<vector>
-// #include<tr1/unordered_map>
-// #include <tr1/unordered_set>
-// //unordered_set相同
-// using namespace std::tr1;
-// //原先的std命名空间也不能省
+#include<vector>
+#include<tr1/unordered_map>
+#include <tr1/unordered_set>
+//unordered_set相同
+using namespace std::tr1;
+//原先的std命名空间也不能省
 using namespace std;
 // // @lc code=start
+//暴力
+// class Solution {
+// public:
+//     std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        
+//         for(int i = 0;i < nums.size();i++){
+//             for(int j = i + 1;j < nums.size();j++){
+//                 if(nums[i] + nums[j] == target){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+
+//         return {0,0};
+
+//     }
+// };
+
+//哈希
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        unordered_map<int, int> hashtable;
-        for (int i = 0; i < nums.size(); ++i) {
-            auto it = hashtable.find(target - nums[i]);
-            if (it != hashtable.end()) {
-                return {it->second, i};
-            }
-            hashtable[nums[i]] = i;
+        
+        unordered_map<int,int>u_map;
+        for(int i = 0;i < nums.size();++i){
+                auto it = u_map.find(target - nums[i]);
+                if(it != u_map.end()){
+                    return {it->second,i};
+                }
+                u_map[nums[i]] = i;
         }
+
         return {};
+
     }
 };
 
